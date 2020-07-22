5481b0fd5ca1631f6e168d836d5efba6d99fbeea
skhumatine <skhumbuzomatine@gmail.com>
2020-07-22 05:00:56 +0200

    initial comit

---
+++ ithemba_walkathon/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ithemba_walkathon project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ithemba_walkathon.settings')
+
+application = get_asgi_application()
