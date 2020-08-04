24a15252acf804b506dc703ce75f389db5c49d67
skhumatine <skhumbuzomatine@gmail.com>
2020-08-04 11:39:33 +0200

    add field to walker

--- walkathon/graphql_schema/mutations.py
+++ walkathon/graphql_schema/mutations.py
@@ -3,6 +3,7 @@ from graphql import GraphQLError
 
 from ithemba_walkathon.graphql_schema.types import UserType
 from ..models import Walker, Walkathon, UserMessages
+from .types import WalkerType
 
 
 # 1 mutation class
@@ -30,6 +31,34 @@ class CreateWalker(graphene.Mutation):
             walker_number=walker.walker_number,
             user_profile=walker.user_profile,
         )
+
+
+# 1 mutation class
+class UpdateWalker(graphene.Mutation):
+    # output of the mutation
+    walker = graphene.Field(WalkerType)
+
+    # 2 data you can send to the server
+    class Arguments:
+        field_name = graphene.String()
+        field_value = graphene.String()
+
+    # 3 mutation method: it creates a Runner in the database using the data sent by the user
+    def mutate(self, info, field_name, field_value):
+        user_profile = info.context.user
+        if user_profile.is_anonymous:
+            raise GraphQLError('You must be logged to create a walker profile!')
+        walker = Walker.objects.filter(user_profile=user_profile).first()
+        walker.distance_to_walk = 8
+        walker.save()
+        print('what came innnnkn\n')
+        print(field_name)
+        print(field_value)
+        print(walker)
+        print('=======================\n')
+
+        # server returns the CreateRunner class with the data just created
+        return Walker.objects.filter(user_profile=user_profile).first()
 
 
 class CreateWalkathon(graphene.Mutation):
@@ -70,3 +99,4 @@ class CreateWalkathon(graphene.Mutation):
 class Mutation(graphene.ObjectType):
     create_walker = CreateWalker.Field()
     create_walkathon = CreateWalkathon.Field()
+    update_walker = UpdateWalker.Field()
